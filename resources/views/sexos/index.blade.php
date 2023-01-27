12356ad1093efe2e1625bfd205fd977a34891117
davidmaya79 <davidmaya79@gmail.com>
2023-01-27 15:08:17 -0300

    index de sexo

--- resources/views/sexos/index.blade.php
+++ resources/views/sexos/index.blade.php
@@ -4,7 +4,45 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12"></div>
-            <h1>Seccion Sexos</h1>
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">Acciones</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Descripción</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center" width="20%">
+                                <a href="" class="btn btn-primary btn-sm shadow-none" 
+                                        data-toggle="tooltip" data-placement="top" title="Ver Sexo">
+                                    <i class="fa fa-book fa-fw text-white"></i></a>
+                                </a>
+                                <a href="" class="btn btn-success btn-sm shadow-none" 
+                                        data-toggle="tooltip" data-placement="top" title="Editar Sexo">
+                                    <i class="fa fa-pencil fa-fw text-white"></i></a>
+                                </a>
+                                <form action="" method="POST" class="d-inline-block">
+                                    <button id="delete" name="delete" type="submit" 
+                                            class="btn btn-danger btn-sm shadow-none" 
+                                            data-toggle="tooltip" data-placement="top" title="Eliminar Sexo"
+                                            onclick="return confirm('¿Estás seguro de eliminar?')">
+                                        <i class="fa fa-trash-o fa-fw"></i>
+                                    </button>
+                                </form>
+                            </td>
+                            <td scope="row">1</td>
+                            <td scope="row">Masculino</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+            
+            
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         </div>
     </div>
 @endsection
